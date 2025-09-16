ae9a89040a3a27d58941b4bda291ef48cdc1e4b9
yatheeshgowda93 <gowdayatheesh93@gmail.com>
2025-09-16 22:06:31 +0530

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/yatheesh-portfolio/",   // ✅ GitHub Pages repo name
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
